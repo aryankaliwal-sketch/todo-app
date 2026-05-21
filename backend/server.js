5377313ad74697783b8cd8f2ed5293429d446406
aryankaliwal_sketch <aryan.kaliwal@gmail.com>
2026-05-20 20:05:42 -0400

    Add Vercel deployment config

--- backend/server.js
+++ backend/server.js
@@ -61,6 +61,10 @@ app.delete('/api/tasks/:id', (req, res) => {
   res.status(204).send();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
